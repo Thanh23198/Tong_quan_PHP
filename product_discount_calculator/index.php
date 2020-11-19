860b4698627fa10eca835be4f9cc5f23e7af0560
Thanh23198 <nguyendinhthanh2311998@gmail.com>
2020-11-19 21:12:03 +0700

    add file

---
+++ product_discount_calculator/index.php
@@ -0,0 +1,16 @@
+<h1 style="color: green">Product Discount Calculator</h1>
+<form method="post" style="border: 1px red;width: 200px; height: 50px">
+    <input type="text" name="money">
+    <input type="submit" value="Calculate">
+</form>
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $money = (float)$_POST["money"]."000";
+    if($money >0){
+        $DiscountAmount = $money*0.8;
+        echo "Sản phẩm bạn mua có giá ".$money." sau khi giảm giá còn ".$DiscountAmount;
+    } else {
+        echo "Bạn đang âm tiền";
+    }
+}
+
